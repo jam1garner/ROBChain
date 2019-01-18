e55751d9084ba043f07c006c01222cbee1bc0db6
jam1garner <jam1.mcleod@hotmail.com>
2019-01-18 16:51:28 -0500

    Move PoC to new build system

--- poc/generate_payload.py
+++ poc/generate_payload.py
@@ -1,4 +1,4 @@
-with open('string.bin', 'rb') as stringFile:
-    with open('string.txt', 'w') as f:
+with open('payload.bin', 'rb') as stringFile:
+    with open('payload.s', 'w') as f:
         for byte in stringFile.read():
             print('byte %s' % hex(byte), file=f)
